decef5f44d1f1dd344e2d7d004bf75e08897b66a
张荣响 <zhangrxiang@yahoo.com>
2017-08-11 16:40:29 +0800

    test

---
+++ mytest/1.c
@@ -0,0 +1,14 @@
+//
+// Created by zhangrxiang on 2017/8/11.
+//
+
+
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    printf("hello world");
+    sleep(10);
+    printf("world hello");
+
+}
